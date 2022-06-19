343f0bcdc77b642e1e6958e8fdbb144bef2a50e1
Nabila <nabila.izatin22.12.2000@gmail.com>
2022-06-19 16:03:16 +0700

    commit 2

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Http\Controllers\Api\GroupsController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\homeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('', [homeController::class, 'index']);
+Route::resources([
+    'friends' => homeController::class,
+    'groups' => GroupsController::class,
+]);
